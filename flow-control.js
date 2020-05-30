a3e53c3e4bfc04c56d3ced3c7d37f4a504d374b7
fortunate-engine-7446 <brenande@gmail.com>
2020-05-30 21:47:19 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,43 @@
 function basicTeenager(age) {
-
+  if (age >= 13) {
+    return "You are a teenager!";
+  }
+basicTeenager(19)
 }
 
 function teenager(age) {
-
+  if (age === 19) {
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager"
+  }
 }
+teenager(12)
 
 function ageChecker(age) {
-
+  if (age < 9) {
+    return "You are a teenager!";
+  }else if (age > 12) {
+    return "You are a grownup";
+  }else {
+    return "You are a kid";
+  }
 }
-
+ageChecker(10)
 function ternaryTeenager(age) {
-
+  return (age <= 19) ? "You are a teenager" : "You are not a teenager";
 }
+ternaryTeenager(16)
 
 function switchAge(age) {
+  switch(age) {
+    case (age > 13) : "You have an age";
+    break;
+    case (age < 19) : "You have an age";
+    break;
+    default:
+    return "You are a teenager";
 
+  }
 }
+switchAge(14);
