17f354e6e10dfa4eaad962a54a1e51b606236025
fortunate-engine-7446 <brenande@gmail.com>
2020-05-30 22:21:19 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,7 +32,19 @@ ternaryTeenager(16)
 
 function switchAge(age) {
   switch(age) {
-    case "13" : return "You are a teenager";
+    case 13 : return "You are a teenager";
+    break;
+    case 14 : return "You are a teenager";
+    break;
+    case 15 : return "You are a teenager";
+    break;
+    case 16 : return "You are a teenager";
+    break;
+    case 17 : return "You are a teenager";
+    break;
+    case 18 : return "You are a teenager";
+    break;
+    case 19 : return "You are a teenager";
     break;
     default:
     return "You have an age";
